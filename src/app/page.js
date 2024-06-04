081ab17b93254acb07cfea6b3770cb4ef6ae253a
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-04 19:44:29 +0530

    setup apollo client

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,43 @@
+"use client";
+import { useQuery } from "@apollo/client";
+import { PRODUCT_QUERY } from "../../lib/query";
+
+import Image from "next/image";
+
 export default function Home() {
+  const { data, loading, error } = useQuery(PRODUCT_QUERY, {
+    variables: { id: 1000 },
+  });
+
+  const products = data?.products.data;
+  console.log(products);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <main>
-      <h1>Welcome to Next.js!</h1>
-    </main>
+    <>
+      <title>Products</title>
+      <main>
+        <h1>Products</h1>
+        {products.map((product) => (
+          <div key={product.attributes.title}>
+            <h2>{product.attributes.title}</h2>
+            <p>{product.attributes.description}</p>
+            <p>{product.attributes.price}</p>
+            <Image
+              src={
+                product.attributes.image.data.attributes.formats.thumbnail.url
+              }
+              alt={product.attributes.title}
+              width={200}
+              height={200}
+              priority
+            />
+          </div>
+        ))}
+      </main>
+    </>
   );
 }
