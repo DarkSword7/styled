47b36b6bcb9be59b53a90852c5181af34e67b5b5
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-05 19:49:14 +0530

    chore: Add Cart component for displaying the shopping cart

---
+++ src/components/Cart.js
@@ -0,0 +1,50 @@
+/* eslint-disable @next/next/no-img-element */
+import { CircleMinus, CirclePlus, ShoppingCart } from "lucide-react";
+import { useStateContext } from "../../lib/context";
+import {
+  CartWrapper,
+  CartStyle,
+  Card,
+  CardInfo,
+  EmptyStyle,
+  Quantity,
+} from "../../styles/CartStyle";
+
+export default function Cart() {
+  const { cartItems, increaseQty, decreaseQty, quantity } = useStateContext();
+
+  return (
+    <CartWrapper>
+      <CartStyle>
+        {cartItems.length < 1 && (
+          <EmptyStyle>
+            <h1>You have more shopping to do 😉</h1>
+            <ShoppingCart />
+          </EmptyStyle>
+        )}
+        {cartItems.length >= 1 &&
+          cartItems.map((item) => (
+            <Card key={item.slug}>
+              <img
+                src={item.image.data.attributes.formats.thumbnail.url}
+                alt={item.title}
+              />
+              <CardInfo>
+                <h3>{item.title}</h3>
+                <h3>{item.price}</h3>
+                <Quantity>
+                  <button onClick={decreaseQty}>
+                    <CircleMinus />
+                  </button>
+                  <p>{item.quantity}</p>
+                  <button onClick={increaseQty}>
+                    <CirclePlus />
+                  </button>
+                </Quantity>
+              </CardInfo>
+            </Card>
+          ))}
+      </CartStyle>
+    </CartWrapper>
+  );
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import { ShoppingBag } from "lucide-react";
 import Link from "next/link";
 import { NavBar, NavItems } from "../../styles/NavStyle";
+import Cart from "./Cart";
 
 import React from "react";
 
@@ -11,10 +12,10 @@ export const Nav = () => {
       <NavItems>
         <div>
           <ShoppingBag />
-
           <h3>Cart</h3>
         </div>
       </NavItems>
+      <Cart />
     </NavBar>
   );
 };
