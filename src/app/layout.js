fafe790a0225b50a69c4fbfcd6198735f8b30ac1
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-05 16:47:31 +0530

    add CartContext to StateContext for managing quantity state

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,19 +4,22 @@ import client from "../../lib/apolloClient";
 import { Inter } from "next/font/google";
 import "../../styles/globals.css";
 import Nav from "@/components/Nav";
+import { StateContext } from "../../lib/context";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <ApolloProvider client={client}>
-      <html lang="en">
-        <title>Next.js with Strapi</title>
-        <body className={inter.className} suppressHydrationWarning={true}>
-          <Nav />
-          {children}
-        </body>
-      </html>
-    </ApolloProvider>
+    <StateContext>
+      <ApolloProvider client={client}>
+        <html lang="en">
+          <title>Next.js with Strapi</title>
+          <body className={inter.className} suppressHydrationWarning={true}>
+            <Nav />
+            {children}
+          </body>
+        </html>
+      </ApolloProvider>
+    </StateContext>
   );
 }
